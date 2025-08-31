6d0978e6e3c8fbf4954c76b6bd4090dc5124fdba
AbhiFestiva <abhishekfestiva@gmail.com>
2025-08-31 19:31:50 +0530

    third

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <>you are a good boy !!!!!</>
+    <>you are a good boy !!!!!
+    <button>click me hard!</button></>
   );
 }
